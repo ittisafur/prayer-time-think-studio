d38aa94a26a3dc546a6f52ba046650f83b2935d9
ittisafur <ittisafur@gmail.com>
2021-05-11 02:59:32 +0600

    Bare minimum skeleton

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,30 @@
 <template>
   <div class="container">
     <h1>Prayer Times</h1>
+    <div v-for="time in currentPrayerTime" :key="time.date.readable">
+        <PrayerTime :times="time.timings" /> 
+    </div>
     <pre v-if="!$fetchState.pending">{{ currentPrayerTime }}</pre>
     <!-- <pre v-if="!$fetchState.pending">{{ prayerTime }}</pre> -->
-    <ul>
-      <li v-for="time in currentPrayerTime">{{ time.timings }}</li>
-    </ul>
+    <!-- <ul>
+      <li v-for="time in currentPrayerTime" :key="time.date.readable">
+        {{ time.timings }}
+      </li>
+    </ul> -->
   </div>
 </template>
 
 <script>
+import PrayerTime from '~/components/PrayerTime'
 export default {
   data() {
     return {
       title: "Prayer times - Think Studio",
       prayerTime: null,
     };
+  },
+  components:{
+    PrayerTime
   },
   head() {
     return {
@@ -32,9 +41,10 @@ export default {
   async fetch() {
     await this.$axios
       .$get(
-        "http://api.aladhan.com/v1/calendar?latitude=23.810331&longitude=90.412521&method=1&month=4&year=2021"
+        "http://api.aladhan.com/v1/calendar?latitude=23.810331&longitude=90.412521&method=1&month=5&year=2021"
       )
-      .then((res) => (this.prayerTime = res.data));
+      .then((res) => (this.prayerTime = res.data))
+      .catch(err => console.log(err))
   },
   computed: {
     currentPrayerTime() {

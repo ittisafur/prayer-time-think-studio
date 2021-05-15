ee1f09413b936d5d2ff192e5fbfa35174be82e44
ittisafur <ittisafur@gmail.com>
2021-05-16 03:02:47 +0600

    Longtitude and Latitude is now being shown in the data property of vue js. Current hurdle is to ensure location loads before the api call

--- pages/index.vue
+++ pages/index.vue
@@ -18,10 +18,44 @@ export default {
     return {
       title: "Prayer times - Think Studio",
       prayerTime: null,
+      geolocation: {
+        lat: 0,
+        lng: 0,
+      },
     };
   },
   components: {
     PrayerTime,
+  },
+  async fetch() {
+    await this.$axios
+      .$get(
+        //`http://api.aladhan.com/v1/calendar?latitude=${this.geolocation.lat}&longitude=${this.geolocation.lng}&method=1&month=5&year=2021`
+        `http://api.aladhan.com/v1/calendar?latitude=23.746&longitude=90.382&method=1&month=5&year=2021`
+      )
+      .then((res) => (this.prayerTime = res.data))
+      .catch((err) => console.log(err));
+  },
+  mounted() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        this.geolocation.lat = pos.coords.latitude;
+        this.geolocation.lng = pos.coords.longitude;
+      });
+    } else {
+      alert("Please enable location detection to pin point prayer times.");
+    }
+  },
+  computed: {
+    currentPrayerTime() {
+      let time = new Date();
+      time.setHours(23, 59, 59, 999);
+      let modifyTime = time.toUTCString();
+      let findCurrentDate = modifyTime.slice(5, 16);
+      return this.prayerTime.filter(
+        (time) => time.date.readable == findCurrentDate
+      );
+    },
   },
   head() {
     return {
@@ -34,25 +68,6 @@ export default {
         },
       ],
     };
-  },
-  async fetch() {
-    await this.$axios
-      .$get(
-        "http://api.aladhan.com/v1/calendar?latitude=23.746&longitude=90.382&method=1&month=5&year=2021"
-      )
-      .then((res) => (this.prayerTime = res.data))
-      .catch((err) => console.log(err));
-  },
-  computed: {
-    currentPrayerTime() {
-      let time = new Date();
-      time.setHours(23, 59, 59, 999);
-      let modifyTime = time.toUTCString();
-      let findCurrentDate = modifyTime.slice(5, 16);
-      return this.prayerTime.filter(
-        (time) => time.date.readable == findCurrentDate
-      );
-    },
   },
 };
 </script>
